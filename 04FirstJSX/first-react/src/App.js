665f55841b4c9721aa0a3a54c742fb6b2e59923a
David Cogbill <d_cogbill@yahoo.com>
2022-04-11 02:49:02 -0400

    [First React With JSX]

---
+++ 04FirstJSX/first-react/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <p>Things I need to do:</p>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Kilimanjaro</li>
+        <li>Run with the bulls</li>
+        <li>Fold Laundry</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
